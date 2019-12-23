550eab809fe1c8faef00ad94cba8366f08b06c44
Alexis <a.manuel@zero-gachis.com>
2019-12-23 16:49:18 +0100

    :white_check_mark: Update unit tests to match Readme examples

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,47 +1,39 @@
 import nurse
 
 
-class SimpleInterface:
-    def simple_abc_method(self):
-        return 'interface result'
-
-
-class SimpleDep:
-    @staticmethod
-    def simple_method():
-        return 'simple dep result'
-
-
-class SimpleDepWithInterface(SimpleInterface):
-    def simple_abc_method(self):
-        return 'impl result'
-
-
-@nurse.inject
-class MyClass:
-    simple_dep: SimpleDep
-    simple_dep_int: SimpleInterface
-
-
 class TestServe:
 
-    def setup(self) -> None:
-        nurse.serve(SimpleDep())
-        nurse.serve(SimpleDepWithInterface(), through=SimpleInterface)
-        self.my_class = MyClass()
+    def test_can_inject_a_dependency(self):
+        class User:
+            @property
+            def name(self):
+                return "Leroy Jenkins"
 
-    def test_simple_dep_instance(self):
-        assert isinstance(self.my_class.simple_dep, SimpleDep)
+        nurse.serve(User())
 
-    def test_dep_inheritance_instance(self):
-        assert isinstance(self.my_class.simple_dep_int, SimpleInterface)
+        @nurse.inject
+        class Game:
+            player: User
 
-    def test_simple_dep_returns_result(self):
-        expected = 'simple dep result'
-        result = self.my_class.simple_dep.simple_method()
-        assert expected == result
+        game = Game()
+        assert game.player.name == "Leroy Jenkins"
 
-    def test_dep_with_interface_returns_impl_result(self):
-        expected = 'impl result'
-        result = self.my_class.simple_dep_int.simple_abc_method()
-        assert expected == result
+    def test_can_inject_a_dependency_through_an_interface(self):
+        class User:
+            @property
+            def name(self):
+                return "Leroy Jenkins"
+
+        class Cheater(User):
+            @property
+            def name(self):
+                return "Igor"
+
+        nurse.serve(Cheater(), through=User)
+
+        @nurse.inject
+        class Game:
+            player: User
+
+        game = Game()
+        assert game.player.name == "Igor"
