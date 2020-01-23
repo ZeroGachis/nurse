bac1b642a0df27145d3d06f9b69a63994eb88828
François PERIN <f.perin@zero-gachis.com>
2020-01-23 13:20:10 +0100

    :sparkles: Bump to 0.4.0

--- nurse/__init__.py
+++ nurse/__init__.py
@@ -1,4 +1,4 @@
 from nurse.api import inject, serve, clear
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __all__ = ['inject', 'serve', 'clear']
--- tests/test_nurse.py
+++
@@ -1,5 +0,0 @@
-from nurse import __version__
-
-
-def test_version():
-    assert __version__ == '0.3.1'
