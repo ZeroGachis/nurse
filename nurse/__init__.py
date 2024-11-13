6010efe7ec6c5ddf1258b60eb336b9ce316b1cde
ducdetronquito <g.paulet@smartway.ai>
2024-11-13 09:57:53 +0100

    feat!: Expose annotations types + make nurse.get() raise when service is not registered

--- nurse/__init__.py
+++ nurse/__init__.py
@@ -1,4 +1,5 @@
-from nurse.api import clear, get, inject, serve
+from nurse.api import clear, get, serve
+from nurse.exceptions import ServiceNotFound
 
 __version__ = "0.5.0"
-__all__ = ["clear", "get", "inject", "serve"]
+__all__ = ["clear", "get", "serve", "ServiceNotFound"]
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,7 @@
+from dataclasses import dataclass
 from unittest import TestCase
 import nurse
 import pytest
-
-from nurse.exceptions import DependencyError
-import asyncio
 
 
 class TestServe(TestCase):
@@ -19,31 +17,33 @@ class TestServe(TestCase):
 
         nurse.serve(User())
 
-        @nurse.inject("player")
+        @dataclass
         class Game:
             player: User
 
-        game = Game()
+        user = nurse.get(User)
+        game = Game(user)
         assert game.player.name == "Leroy Jenkins"
 
     def test_can_inject_a_dependency_through_an_interface(self):
         class User:
             @property
-            def name(self):
+            def name(self) -> str:
                 return "Leroy Jenkins"
 
         class Cheater(User):
             @property
-            def name(self):
+            def name(self) -> str:
                 return "Igor"
 
         nurse.serve(Cheater(), through=User)
 
-        @nurse.inject("player")
+        @dataclass
         class Game:
             player: User
 
-        game = Game()
+        user = nurse.get(User)
+        game = Game(user)
         assert game.player.name == "Igor"
 
     def test_cannot_serve_a_dependency_if_it_does_not_subclass_the_provided_interface(
@@ -51,12 +51,12 @@ class TestServe(TestCase):
     ):
         class User:
             @property
-            def name(self):
+            def name(self) -> str:
                 return "Leroy Jenkins"
 
         class Animal:
             @property
-            def name(self):
+            def name(self) -> str:
                 return "Miaouss"
 
         with pytest.raises(ValueError):
@@ -64,67 +64,11 @@ class TestServe(TestCase):
 
 
 class ServiceDependency:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.name
-
-
-class TestInjectMethod(TestCase):
-    def tearDown(self):
-        super().tearDown()
-        nurse.clear()
-
-    def test_methods_with_only_dependency_args(self):
-        @nurse.inject("service")
-        def foo(service: ServiceDependency):
-            return service.get_name()
-
-        nurse.serve(ServiceDependency("Leroy Jenkins"))
-
-        assert foo() == "Leroy Jenkins"
-
-    def test_methods_with_dependency_and_value_args(self):
-        @nurse.inject("service")
-        def foo(prefix, service: ServiceDependency):
-            return prefix + service.get_name()
-
-        nurse.serve(ServiceDependency("Leroy Jenkins"))
-
-        assert foo("My name is ") == "My name is Leroy Jenkins"
-
-    def test_methods_with_missing_type(self):
-        @nurse.inject("service")
-        def foo(service):
-            pass
-
-        with pytest.raises(
-            DependencyError, match="Args `service` must be typed to be injected."
-        ):
-            foo()
-
-    def test_methods_with_missing_dependency(self):
-        @nurse.inject("service")
-        def foo(service: ServiceDependency):
-            pass
-
-        with pytest.raises(
-            DependencyError,
-            match="Dependency `ServiceDependency` for `service` was not found.",
-        ):
-            foo()
-
-    def test_async_method(self):
-        @nurse.inject("service")
-        async def foo(service: ServiceDependency):
-            return service.get_name()
-
-        nurse.serve(ServiceDependency("Leroy Jenkins"))
-
-        res = asyncio.run(foo())
-
-        assert res == "Leroy Jenkins"
 
 
 class TestGet(TestCase):
@@ -136,8 +80,9 @@ class TestGet(TestCase):
         nurse.serve(ServiceDependency("Leroy Jenkins"))
 
         service = nurse.get(ServiceDependency)
+        assert service is not None
         assert service.get_name() == "Leroy Jenkins"
 
     def test_returns_none_if_service_is_not_registered(self):
-        service = nurse.get(ServiceDependency)
-        assert service is None
+        with pytest.raises(nurse.ServiceNotFound):
+            nurse.get(ServiceDependency)
